1a71e77303bac06f8855463276fc0ff0d7595637
Dhananjay Narula <dhananjay0376@gmail.com>
2026-04-11 23:05:34 +0530

    feat: add db package scaffold

---
+++ packages/db/src/index.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+const { drizzle } = require("drizzle-orm/node-postgres");
+const schema = require("./schema");
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const sslEnabled = process.env.DATABASE_SSL === "true";
+
+const pool = new Pool({
+  connectionString,
+  ssl: sslEnabled ? { rejectUnauthorized: false } : undefined,
+});
+
+const db = drizzle(pool, { schema });
+
+module.exports = { db, pool, schema };
+
